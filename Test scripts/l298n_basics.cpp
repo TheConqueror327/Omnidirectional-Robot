229e91182e1c9273f234e0c2c9ceb5f1141702ba
TheConqueror327 <mark.erdohelyi@gmail.com>
2024-06-03 19:16:02 +0200

    Create l298n_basics.cpp

---
+++ Test scripts/l298n_basics.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+
+#define enA D3
+#define in1 D4
+#define in2 D5
+
+void setup() {
+	pinMode(enA, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+}
+
+void directionControl() {
+	analogWrite(enA, 255);
+
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+
+	delay(2000);
+	
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+
+	delay(2000);
+	
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+}
+
+void speedControl() {
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+
+	for (int i = 0; i <= 255; i++) {
+		analogWrite(enA, i);
+		delay(20);
+	}
+	
+	for (int i = 255; i >= 0; --i) {
+		analogWrite(enA, i);
+		delay(20);
+	}
+	
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+}
+
+void loop() {
+	directionControl();
+	delay(1000);
+	speedControl();
+	delay(1000);
+}
